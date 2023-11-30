d2ea3ef5d654af68329f02f43e9e0d91cf1abd5c
yeeeeechan <leeyechan6@gmail.com>
2023-11-30 21:07:31 +0900

    🐛 코드 수정

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -3,13 +3,13 @@ const controller = require("../controllers/Cshop");
 const router = express.Router();
 
 // 카테고리별 상품 페이지
-router.get("/category/:categoryName", controller.categoryPage);
+router.get("/category/:categoryId", controller.categoryPage);
 
 // 상품 상세 페이지
 router.get("/product/:productId", controller.productPage);
 
 // 장바구니 담기
-router.post("/payment/:productsId", controller.shop);
+router.post("/payment/:productId", controller.cartIn);
 
 // 결제하기
 router.get("/payment", controller.payment);
