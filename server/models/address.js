827c0e9504865d0092e58adf4d75273f9996c3f3
yeeeeechan <leeyechan6@gmail.com>
2023-12-08 17:57:16 +0900

    ✨ db onDelete 설정 추가

--- server/models/address.js
+++ server/models/address.js
@@ -1,44 +1,45 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('address', {
-    addressId: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "address",
+    {
+      addressId: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "userNumber",
+        },
+        onDelete: "CASCADE",
+      },
+      address: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
     },
-    userNumber: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'userNumber'
-      }
-    },
-    address: {
-      type: DataTypes.STRING(200),
-      allowNull: false
+    {
+      sequelize,
+      tableName: "address",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "addressId" }],
+        },
+        {
+          name: "userNumber",
+          using: "BTREE",
+          fields: [{ name: "userNumber" }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'address',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "addressId" },
-        ]
-      },
-      {
-        name: "userNumber",
-        using: "BTREE",
-        fields: [
-          { name: "userNumber" },
-        ]
-      },
-    ]
-  });
+  );
 };
--- server/models/carts.js
+++ server/models/carts.js
@@ -1,59 +1,58 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('carts', {
-    cartId: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "carts",
+    {
+      cartId: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "userNumber",
+        },
+        onDelete: "CASCADE",
+      },
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "products",
+          key: "productId",
+        },
+      },
+      cartQuantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    userNumber: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'userNumber'
-      }
-    },
-    productId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'products',
-        key: 'productId'
-      }
-    },
-    cartQuantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+    {
+      sequelize,
+      tableName: "carts",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "cartId" }],
+        },
+        {
+          name: "userNumber",
+          using: "BTREE",
+          fields: [{ name: "userNumber" }],
+        },
+        {
+          name: "productId",
+          using: "BTREE",
+          fields: [{ name: "productId" }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'carts',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "cartId" },
-        ]
-      },
-      {
-        name: "userNumber",
-        using: "BTREE",
-        fields: [
-          { name: "userNumber" },
-        ]
-      },
-      {
-        name: "productId",
-        using: "BTREE",
-        fields: [
-          { name: "productId" },
-        ]
-      },
-    ]
-  });
+  );
 };
