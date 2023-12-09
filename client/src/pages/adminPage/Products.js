9fba7cd63edc0e056aa71d23cd0827c53edeee1b
hyeoonjeoong <jeooooong22@gmail.com>
2023-12-10 01:44:14 +0900

    ✨ 테이블 컴포넌트 적용

--- client/src/pages/adminPage/Products.js
+++ client/src/pages/adminPage/Products.js
@@ -62,6 +62,15 @@ function Products() {
     <>
       <Card>
         <h3>상품 관리</h3>
+        <ol>
+          <li>
+            ✅체크 박스로 상품 선택 가능하도록 구현 (들어갔으면 하는 요소 -
+            "전체 선택", "선택 삭제")
+          </li>
+          <li>✅전체상품 조회 페이지 내 "상품 등록" button 필요</li>
+          <li>✅productStatus : "판매중" or "상품준비중" - select 요소로</li>
+          <li>✅10개씩 보여주기 페이지네이션</li>
+        </ol>
         <DataTable headers={header} items={DUMMY} />
       </Card>
     </>
--- client/src/pages/adminPage/Users.js
+++ client/src/pages/adminPage/Users.js
@@ -3,13 +3,62 @@ import React from "react";
 import * as S from "../../styles/adminPage/Users.js";
 
 import Card from "../../shared/adminPage/components/Card";
+import DataTable from "../../shared/adminPage/components/DataTable";
+
+const header = [
+  {
+    text: "NO.",
+    value: "userNumber",
+  },
+  {
+    text: "아이디",
+    value: "userId",
+  },
+  {
+    text: "이름",
+    value: "userName",
+  },
+  {
+    text: "핸드폰 번호",
+    value: "phoneNumber",
+  },
+  {
+    text: "생년월일",
+    value: "birthday",
+  },
+  {
+    text: "성별",
+    value: "gender",
+  },
+  {
+    text: "주소",
+    value: "address",
+  },
+];
+
+const DUMMY = [
+  {
+    userNumber: 1,
+    userId: "che",
+    userName: "내이름",
+    phoneNumber: "01000000000",
+    birthday: "1995-08-11",
+    gender: "여",
+    address: "서울시 구로구 ",
+  },
+];
 
 function Users() {
   return (
     <>
       <Card>
-        <h3>Users 회원관리</h3>
-        <h3>회원없슴</h3>
+        <h3>회원 관리</h3>
+        <ol>
+          <li>✅체크박스로 선택 가능하도록 (전체선택, 회원삭제 버튼)</li>
+          <li>✅상세조회는 없음</li>
+          <li>✅10명씩. 페이지네이션</li>
+        </ol>
+        <DataTable headers={header} items={DUMMY} />
       </Card>
     </>
   );
