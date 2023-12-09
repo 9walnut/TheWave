eb29d2a4e1f2802d558e7dbd58335ebb707ef9f8
hyeoonjeoong <jeooooong22@gmail.com>
2023-12-10 01:54:45 +0900

    ➕ 필요한 파일 추가

---
+++ client/src/pages/adminPage/ProductsAdd.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import * as S from "../../styles/adminPage/ProductsAdd.js";
+
+function ProductsAdd() {
+  return (
+    <>
+      <h3>상품 등록 페이지</h3>
+      <h3>products/add</h3>
+    </>
+  );
+}
+
+export default ProductsAdd;
