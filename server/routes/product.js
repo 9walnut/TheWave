78b1ffc846855d1a846e310abe352a44c1a721f5
yeeeeechan <leeyechan6@gmail.com>
2023-12-10 14:02:35 +0900

    commit change

--- server/routes/product.js
+++ server/routes/product.js
@@ -8,4 +8,9 @@ router.get("/:productId", controller.productPage);
 // 장바구니 담기
 router.post("/:productId", controller.cartIn);
 
+// 장바구니 > '계속 쇼핑하기' 클릭 시
+
+// 장바구니 > '장바구니 보기' 클릭 시
+router.get("/:productId", controller.goCart);
+
 module.exports = router;
