5e226a542c15c36c0a1dfba6a03eb14174150123
hyeoonjeoong <jeooooong22@gmail.com>
2023-12-15 02:15:48 +0900

    ✨ ProductsDetail 라우터 추가

--- client/src/pages/adminPage/ProductsDetail.js
+++ client/src/pages/adminPage/ProductsDetail.js
@@ -6,7 +6,8 @@ function ProductsDetail() {
   return (
     <>
       <h3>상품 상세 페이지</h3>
-      <h3>products/{productsID}</h3>
+      <h3>데이터를 가져와서 쿼리스트링으로 사용해야할거같은데..? </h3>
+      <h3>일단 모르겠으니 Route path="products/:productsID" </h3>
     </>
   );
 }
