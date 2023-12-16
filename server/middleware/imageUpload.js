f529071bb602f177f4e6c29cd39003688a199cfa
9walnut <kguho9202@gmail.com>
2023-12-16 15:54:30 +0900

    ✨ 인증서 추가

--- server/middleware/imageUpload.js
+++ server/middleware/imageUpload.js
@@ -1,12 +1,15 @@
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const multer3 = require("multer-s3");
+const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
+
 AWS.config.update({
-  region: "ap-northeast-2", // region 값
-  accessKeyId: "AKIA6FD5HWZ6243X6SHZ", // accessKeyId
-  secretAccessKey: "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", // secretAccessKey
+  region: process.env.AWS_REGION, // region 값
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // accessKeyId
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // secretAccessKey
 });
 
 const s3 = new AWS.S3();
@@ -15,7 +18,7 @@ const allowedExtensions = [".png", ".jpg", ".jpeg", ".bmp"];
 const thumbnailUpload = multer({
   storage: multer3({
     s3: s3,
-    bucket: "thewave-bucket", // bucket-name
+    bucket: process.env.S3_BUCKET_NAME, // bucket-name
     key: (req, file, callback) => {
       const uploadDirectory = "thumbnails";
       const extension = path.extname(file.originalname);
@@ -32,7 +35,7 @@ const thumbnailUpload = multer({
 const detailUpload = multer({
   storage: multer3({
     s3: s3,
-    bucket: "thewave-bucket", // bucket-name
+    bucket: process.env.S3_BUCKET_NAME, // bucket-name
     key: (req, file, callback) => {
       const uploadDirectory = "details";
       const extension = path.extname(file.originalname);
