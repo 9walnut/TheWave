a710b303368ca155e832291903c20078e2df7ab7
9walnut <kguho9202@gmail.com>
2023-12-27 14:45:31 +0900

    ✨ db 연결 테스트

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
+    host: "mysql_new",
+    database: "thewave",
+    username: "admin",
+    password: "1q2w3e4r",
     dialect: "mysql",
   },
 };
