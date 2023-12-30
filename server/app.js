7611f540e40d6fdfc78f6d4a96edf20094edb022
9walnut <kguho9202@gmail.com>
2023-12-30 21:18:14 +0900

    be : https

--- server/app.js
+++ server/app.js
@@ -1,42 +1,32 @@
 const http = require("http");
-// const https = require("https");
-// const fs = require("fs");
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const redis = require("redis");
 const client = redis.createClient({
-  host: process.env.REDIS_HOST, // 'redis-19442.c323.us-east-1-2.ec2.cloud.redislabs.com'
-  port: process.env.REDIS_PORT, // '19442'
-  // 필요하다면 사용자 이름과 비밀번호도 추가
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 const cors = require("cors");
 const path = require("path");
 const session = require("express-session");
 const PORT = 8000;
-// const https_port = 8001;
+const https_port = 8001;
 
 // const options = {
 //   key: fs.readFileSync("./rootca.key"),
 //   cert: fs.readFileSync("./rootca.crt"),
 // };
 
-// const options = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/thewavemarket.com/privkey.pem"),
-//   cert: fs.readFileSync(
-//     "/etc/letsencrypt/live/thewavemarket.com/fullchain.pem"
-//   ),
-// };
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/thewavemarket.co.kr/privkey.pem"),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/thewavemarket.co.kr/fullchain.pem"
+  ),
+};
 const server = http.createServer(app);
-// const https_server = https.createServer(options, app);
-
-// const io = require("socket.io")(server);
-// const io_https = require("socket.io")(https_server);
-
-// 실시간 채팅 미들웨어
-// const { liveChat } = require("./middleware/liveChat");
-
-// liveChat(io);
-// socketHandler(io_https);
+const https_server = https.createServer(options, app);
 
 app.use(cors());
 app.use(express.json());
@@ -77,18 +67,10 @@ app.use("/admin", adminRouter);
 const cartRouter = require("./routes/cart");
 app.use("/cart", cartRouter);
 
-// io.on("connection", (socket) => {
-//   console.log("New connection via HTTP");
-// });
-
-// io_https.on("connection", (socket) => {
-//   console.log("New connection via HTTPS");
-// });
-
 server.listen(PORT, function () {
   console.log(`Sever Open: ${PORT}`);
 });
 
-// https_server.listen(https_port, function () {
-//   console.log(`HTTPS Server Open: ${https_port}`);
-// });
+https_server.listen(https_port, function () {
+  console.log(`HTTPS Server Open: ${https_port}`);
+});
