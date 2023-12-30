beeb813e1e117244ed7a08d223281b147bfa4f7b
dntjd129 <dntjd129@naver.com>
2023-12-30 14:05:18 +0900

    ✨ 카테고리 상세 추가

--- client/src/pages/mainPage/products/CategoryProducts.js
+++ client/src/pages/mainPage/products/CategoryProducts.js
@@ -1,13 +1,15 @@
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import Navbar from "../../../components/mainPage/Navbar";
 import Footer from "../../../components/mainPage/Footer";
 import "../MainPage.css";
 import { useEffect, useState } from "react";
-import * as S from "../../../styles/mainPage/CategoryProducts.style";
+import * as S from "../../../styles/mainPage/ProductCard.style";
 import axios from "axios";
 
 function CategoryProducts() {
   const [products, setProducts] = useState([]);
+  const [categoryName, setCategoryName] = useState("");
   const { categoryId } = useParams();
 
   console.log("카테고리 아이디", categoryId);
@@ -15,8 +17,10 @@ function CategoryProducts() {
   const getCategoryProduct = async () => {
     try {
       const res = await axios.get(`/api/category/${categoryId}`);
-      console.log("카테고리 상품", res.data);
-      setProducts(res.data);
+      console.log("카테고리", res.data[0].categoryName);
+      console.log("상품들", res.data[0].products);
+      setCategoryName(res.data[0].categoryName);
+      setProducts(res.data[0].products);
     } catch (error) {
       console.log("카테고리 불러오기 에러", error);
     }
@@ -24,9 +28,33 @@ function CategoryProducts() {
 
   useEffect(() => {
     getCategoryProduct();
-  }, []);
+  }, [categoryId]);
 
-  return <></>;
+  return (
+    <>
+      <S.ProductContentBox>
+        <div>{categoryName}</div>
+        {products.map((product) => {
+          return (
+            <S.CardItemBox>
+              <Link to={`/products/${product.productId}`}>
+                <div>
+                  <img src={product.thumbnailUrl} />
+                </div>
+                <div>
+                  <ul>
+                    <li>{product.productName}</li>
+                    <li>{product.productInfo}</li>
+                    <li>{product.productPrice}원</li>
+                  </ul>
+                </div>
+              </Link>
+            </S.CardItemBox>
+          );
+        })}
+      </S.ProductContentBox>
+    </>
+  );
 }
 
 export default CategoryProducts;
