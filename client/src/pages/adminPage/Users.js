d0772d6db7da0282f15e11a2a961f6a1c5a2b61a
9walnut <kguho9202@gmail.com>
2024-01-02 11:41:39 +0900

    test

--- client/src/pages/adminPage/Users.js
+++ client/src/pages/adminPage/Users.js
@@ -44,9 +44,7 @@ function Users() {
   //---axios get
   const fetchData = async () => {
     try {
-      const response = await axios.get(
-        "https://thewavemarket.co.kr:8000/api/admin/users"
-      );
+      const response = await axios.get("/api/admin/users");
       console.log("response", response.data);
 
       const modifiedData = response.data.map((user) => ({
