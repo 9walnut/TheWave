e7fc02167927ba09517506b5a60a6deda482544f
9walnut <kguho9202@gmail.com>
2024-01-04 18:24:13 +0900

    ✨ 찾기 기능 추가

--- client/src/pages/mainPage/Registers/FindIdPage.js
+++ client/src/pages/mainPage/Registers/FindIdPage.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Navbar from "../../../components/mainPage/Navbar";
 import Footer from "../../../components/mainPage/Footer";
@@ -7,8 +8,65 @@ import "../MainPage.css";
 // import PageName from "../../../components/register/PageName";
 // import PageInput from "../../../components/register/PageInput";
 import * as S from "./FindIdPageStyle.js";
+import axios from "axios";
 
 function FindIdPage() {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [userId, setUserId] = useState(null);
+  const [notFound, setNotFound] = useState(false);
+
+  const userNameHandler = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const phoneNumberHandler = (e) => {
+    setphoneNumber(e.target.value);
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      if (userName === "" || phoneNumber === "") {
+        alert("이름 또는 핸드폰번호를 입력해주세요.");
+      } else {
+        handleFindId();
+      }
+    }
+  };
+
+  const handleFindId = async () => {
+    const data = {
+      userName: userName,
+      phoneNumber: phoneNumber,
+    };
+    try {
+      const res = await axios.post("/api/findId", data);
+      if (res.data.result === false) {
+        // 일치하는 정보가 없을 때
+        setNotFound(true);
+      } else {
+        // 일치하는 정보가 있을 때
+        setUserId(res.data.userId); // 아이디 값을 상태에 저장
+        setNotFound(false);
+      }
+    } catch (error) {
+      console.log("아이디 찾기", error);
+    }
+  };
+
+  const handleLoginPage = () => {
+    navigate("/login");
+  };
+
+  const handleFindPwPage = () => {
+    navigate("/findPw");
+  };
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
   return (
     <>
       <Navbar />
@@ -18,27 +76,45 @@ function FindIdPage() {
             onSubmit={(e) => {
               e.preventDefault();
             }}
-            // onKeyDown={handleEnter}
+            onKeyDown={handleEnter}
           >
             <S.FindIdHead>아이디 찾기</S.FindIdHead>
             <br />
             <S.Input
-              type="password"
-              // onChange={onPasswordHandler}
+              type="text"
+              onChange={userNameHandler}
               placeholder="이름"
             />
             <br />
             <S.Input
               type="password"
-              // onChange={onPasswordHandler}
+              onChange={phoneNumberHandler}
               placeholder="휴대폰 번호"
             />
             <br />
-            <S.Button
-            // onClick={handleLogin}
-            >
-              아이디 찾기
-            </S.Button>
+            <S.Button onClick={handleFindId}>아이디 찾기</S.Button>
+            <br />
+            <S.ButtonContainer>
+              <S.ButtonGroup>
+                <S.ButtonSecond onClick={handleLoginPage}>
+                  로그인
+                </S.ButtonSecond>
+                <S.ButtonSecond onClick={handleFindPwPage}>
+                  비밀번호 찾기
+                </S.ButtonSecond>
+                <S.ButtonSecond onClick={handleRegister}>
+                  회원가입
+                </S.ButtonSecond>
+              </S.ButtonGroup>
+            </S.ButtonContainer>
+            {userId && (
+              <S.UserIdDisplay>
+                회원님의 아이디는 {userId} 입니다.
+              </S.UserIdDisplay>
+            )}
+            {notFound && (
+              <S.UserIdDisplay>일치하는 정보가 없습니다.</S.UserIdDisplay>
+            )}
           </form>
         </S.FormBox>
       </section>
--- client/src/pages/mainPage/Registers/FindPwPage.js
+++ client/src/pages/mainPage/Registers/FindPwPage.js
@@ -1,14 +1,71 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../../components/mainPage/Navbar";
 import Footer from "../../../components/mainPage/Footer";
 import "../MainPage.css";
-import Button from "../../../components/register/Button";
-import PageName from "../../../components/register/PageName";
-import PageInput from "../../../components/register/PageInput";
+// import Button from "../../../components/register/Button";
+// import PageName from "../../../components/register/PageName";
+// import PageInput from "../../../components/register/PageInput";
 import * as S from "./FindPwPageStyle.js";
+import axios from "axios";
 
 function FindPwPage() {
+  const [userId, setUserId] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [notFound, setNotFound] = useState(false);
+
+  const userIdHandler = (e) => {
+    setUserId(e.target.value);
+  };
+
+  const phoneNumberHandler = (e) => {
+    setphoneNumber(e.target.value);
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      if (userId === "" || phoneNumber === "") {
+        alert("이름 또는 핸드폰번호를 입력해주세요.");
+      } else {
+        handleFindPw();
+      }
+    }
+  };
+
+  const handleFindPw = async () => {
+    const data = {
+      userId: userId,
+      phoneNumber: phoneNumber,
+    };
+    try {
+      const res = await axios.post("/api/findPw", data);
+      if (res.data.result === false) {
+        // 일치하는 정보가 없을 때
+        setNotFound(true);
+      } else {
+        // 일치하는 정보가 있을 때
+        setUserId(res.data.userId); // 아이디 값을 상태에 저장
+        setNotFound(false);
+        navigate("/findPw/newPw");
+      }
+    } catch (error) {
+      console.log("아이디 찾기", error);
+    }
+  };
+
+  const navigate = useNavigate();
+  const handleLoginPage = () => {
+    navigate("/login");
+  };
+
+  const handleFindIdPage = () => {
+    navigate("/findId");
+  };
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
   return (
     <>
       <Navbar />
@@ -18,27 +75,39 @@ function FindPwPage() {
             onSubmit={(e) => {
               e.preventDefault();
             }}
-            // onKeyDown={handleEnter}
+            onKeyDown={handleEnter}
           >
             <S.FindPwHead>비밀번호 찾기</S.FindPwHead>
             <br />
             <S.Input
               type="text"
-              // onChange={onUserIdHandler}
+              onChange={userIdHandler}
               placeholder="아이디"
             />
             <br />
             <S.Input
               type="password"
-              // onChange={onPasswordHandler}
+              onChange={phoneNumberHandler}
               placeholder="휴대폰 번호"
             />
             <br />
-            <S.Button
-            // onClick={handleLogin}
-            >
-              <Link to={"/findPw/newPw"}>비밀번호 찾기</Link>
-            </S.Button>
+            <S.Button onClick={handleFindPw}>비밀번호 찾기</S.Button>
+            <S.ButtonContainer>
+              <S.ButtonGroup>
+                <S.ButtonSecond onClick={handleLoginPage}>
+                  로그인
+                </S.ButtonSecond>
+                <S.ButtonSecond onClick={handleFindIdPage}>
+                  아이디 찾기
+                </S.ButtonSecond>
+                <S.ButtonSecond onClick={handleRegister}>
+                  회원가입
+                </S.ButtonSecond>
+              </S.ButtonGroup>
+            </S.ButtonContainer>
+            {notFound && (
+              <S.UserIdDisplay>일치하는 정보가 없습니다.</S.UserIdDisplay>
+            )}
           </form>
         </S.FormBox>
       </section>
