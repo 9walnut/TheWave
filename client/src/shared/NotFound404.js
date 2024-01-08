b3dace8adccea486691eb80db5a1eeeb400eab7b
hyeoonjeoong <jeooooong22@gmail.com>
2024-01-09 02:14:56 +0900

    🎨 404 희망사항..

--- client/src/shared/NotFound404.js
+++ client/src/shared/NotFound404.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function NotFound() {
-  return <h3>404 error</h3>;
+  return (
+    <>
+      <h3>404 error</h3>;<p>접근할 수 없는 주소입니다!</p>
+      <p>메인화면으로 돌아가기 버튼</p>
+      <p>이 상품은 어떠세요!? 상품 구경하러 가기 버튼, 카드 (희망사항..)</p>
+    </>
+  );
 }
 
 export default NotFound;
