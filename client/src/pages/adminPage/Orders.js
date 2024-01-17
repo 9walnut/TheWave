a17e91a9569532b5e8c9ee367fd66d95286e5f94
hyeoonjeoong <jeooooong22@gmail.com>
2024-01-18 01:33:35 +0900

    ✨ 검색 기능 추가, 오류 수정

--- client/src/pages/adminPage/Orders.js
+++ client/src/pages/adminPage/Orders.js
@@ -11,6 +11,8 @@ import PageNationFunc from "../../shared/PageNationFunc.js";
 import SelectBoxDelivery from "../../shared/adminPage/components/SelectBoxDelivery.js";
 import OrdersDetail from "./OrdersDetail.js";
 import ModifiedPhoneNumber from "../../shared/ModifiedPhoneNumber.js";
+import AdminButtonSearch from "../../components/adminPage/AdminButtonSearch.js";
+import AdminInputSearch from "../../shared/adminPage/components/AdminInputSearch.js";
 
 const header = [
   {
@@ -54,6 +56,10 @@ function Orders() {
   const [selectedOrderId, setSelectedOrderId] = useState(null); //*
   const params = useParams(); //*
 
+  const [searchText, setSearchText] = useState("");
+  const [searchType, setSearchType] = useState("");
+  const [searchData, setSearchData] = useState([]);
+
   const descendingData = (a, b) => {
     return b.orderId - a.orderId;
   };
@@ -78,7 +84,7 @@ function Orders() {
   const fetchData = async () => {
     try {
       const response = await axios.get("/api/admin/orders");
-      // console.log("orders response", response.data);
+      console.log("orders response", response.data);
 
       const modifiedData = response.data.map((order) => ({
         orderId: order.orderId,
@@ -97,6 +103,7 @@ function Orders() {
       modifiedData.sort(descendingData);
 
       setOrders(modifiedData);
+      setSearchData(modifiedData);
       // console.log(modifiedData); //orderStatus 한글로 변경 완료
     } catch (error) {
       console.log("에러", error);
@@ -147,10 +154,66 @@ function Orders() {
     }
   };
 
+  //---searchItem
+  const searchItem = () => {
+    console.log(searchType);
+
+    if (searchText.length === 0) {
+      alert("검색 내용을 입력해주세요");
+      return;
+    }
+
+    const searchItems = searchData.filter((value) => {
+      const inputSearchText = searchText;
+
+      const userNumberUser = value.userNumber_user;
+      console.log("userNumberUser", userNumberUser);
+
+      const selectSearchType =
+        searchType === "userName"
+          ? value.userName
+          : userNumberUser?.phoneNumber;
+
+      console.log("selectSearchType", selectSearchType);
+
+      // return selectSearchType.includes(inputSearchText);
+      return selectSearchType && selectSearchType.includes(inputSearchText);
+    });
+
+    console.log("searchData", searchData);
+    console.log("searchItems", searchItems);
+
+    setSearchText("");
+    setOrders(searchItems);
+  };
+
+  const allItem = () => {
+    fetchData();
+  };
+
   return (
     <>
       <Card>
         <S.InnerCardTitleBox>거래 내역 관리</S.InnerCardTitleBox>
+        <S.SearchButtonContainer>
+          <select
+            name="search"
+            value={searchType}
+            onChange={(e) => setSearchType(e.target.value)}
+          >
+            <option value="userName">주문자</option>
+            <option value="phoneNumber">핸드폰 번호</option>
+          </select>
+          <AdminInputSearch
+            type="text"
+            placeholder="검색어를 입력 해주세요."
+            value={searchText}
+            onChange={(value) => setSearchText(value)}
+          />
+
+          <AdminButtonSearch onClick={searchItem}>검색하기</AdminButtonSearch>
+          <AdminButtonSearch onClick={allItem}>전체보기</AdminButtonSearch>
+        </S.SearchButtonContainer>
         <DataTable
           keySet="ordersTb_"
           headers={header}
